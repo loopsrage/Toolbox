1474deda5d7614cfc375aeb8cf33f67eb85d2ccd
Jarek <Jarek@DESKTOP-7B71UNP>
2017-08-16 01:38:13 -0600

    Updated

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            RichTextBox RTB = new RichTextBox();
+            Button BTN = new Button();
+            ContainerControl CC = new ContainerControl();
+            CC.Location = new Point(0,0);
+            RTB.Location = new Point(0,Bottom);
+            BTN.Location = new Point(Right,Top);
+
+            CC.Height = 500;
+            CC.Width = 500;
+
+            BTN.AutoSize = true;
+            BTN.Text = "Test Json";
+            BTN.Click += (sender, EventArgs) => { BTN_Json(sender, EventArgs, RTB); };
+
+            RTB.Width = CC.Width;
+            RTB.Height = CC.Height / 2;
+            RTB.Text = "JsonValuesHere";
+            RTB.Name = "JsonUserInput";
+
+            this.AutoSize = true;
+
+            this.Controls.Add(CC);
+
+            CC.Controls.Add(RTB);
+            CC.Controls.Add(BTN);
+
+            InitializeComponent();
+        }
+
+        private void BTN_Json(Object sender, EventArgs e,RichTextBox EVT_RTB)
+        {
+            ParseJsonObject PJO = new ParseJsonObject();
+            JsonObject JO = new JsonObject();
+            JO.InString = EVT_RTB.Text;
+            JO.ParseJson();
+            if (JO.JsonEx != null)
+            {
+                Console.Write(JO.JsonEx.Message);
+            }
+            else
+            {
+                List<JsonParseResults> ResultsList = PJO.JObjectParse(JO.OutObject);
+                foreach (JsonParseResults JPR in ResultsList.Where(x => x.Name !=null))
+                {
+                    Console.Write("-Name- {0} -Value- {1} -Type- {2}\r\n",JPR.Name,JPR.Value,JPR.Type);
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Class1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Json Library
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Serialization;
+
+
+namespace WindowsFormsApplication3
+{
+    public class JsonObject
+    {
+        public String InString { get; set; }
+        public JObject OutObject { get; set; }
+        public JsonException JsonEx;
+        public void ParseJson()
+        {
+            try
+            {
+                OutObject = JObject.Parse(InString);
+            }
+            catch (JsonException JsonError)
+            {
+                JsonEx = JsonError;
+            }
+        }
+    }
+    public class JsonParseResults
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Type { get; set; }
+        public string path { get; set; }
+        public JsonParseResults()
+        {
+
+        }
+    }
+
+    public class ParseJsonObject
+    {
+        public string Name;
+        public JToken Data;
+        public List<JsonParseResults> Results = new List<JsonParseResults>();
+        public JsonParseResults R { get; set; }
+        public List<JsonParseResults> JObjectParse(JObject JsonObj)
+        {
+            IEnumerable<String> Paths = JsonObj.DescendantsAndSelf().Where(y => y.HasValues).Select(x => x.Path).Distinct();
+            foreach (string P in Paths)
+            {
+                R = new JsonParseResults();
+                JToken SelectedToken = JsonObj.SelectToken(P);
+                string type = SelectedToken.Type.ToString();
+                R.path = P;
+                R.Type = type;
+                R = HandleType(SelectedToken, R.Type);
+
+                Results.Add(R);
+            }
+            return Results;
+        }
+        public JsonParseResults HandleType(JToken SelectedToken, string type)
+        {
+            type = type.ToLower();
+            switch (type)
+            {
+                case "string":
+                    try
+                    {
+                        R.Name = R.path.Substring(R.path.LastIndexOf('.') + 1);
+                    }
+                    catch
+                    {
+                        R.Name = R.path;
+                    }
+                    R.Value = SelectedToken.ToString();
+                    break;
+                case "object":
+                case "array":
+                    foreach (var AV in SelectedToken.Values())
+                    {
+                        foreach (JToken JT in AV.Values().ToList())
+                        {
+                            HandleType(JT, JT.Type.ToString());
+                        }
+                    }
+                    break;
+            }
+            return R;
+        }
+    }
+
+}
